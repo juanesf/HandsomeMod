81bf731b42616272e03c90b80abab4c83a3aacba
juanesf <juanesf91@gmail.com>
2021-04-11 01:50:04 -0400

    Update cortexa7.mk
    - Add PineCube support

--- target/linux/sunxi/image/cortexa7.mk
+++ target/linux/sunxi/image/cortexa7.mk
@@ -265,3 +265,11 @@ define Device/sinlinx_sinlinx-sina33-v3
 endef
 
 TARGET_DEVICES += sinlinx_sinlinx-sina33-v3
+
+define Device/pinecube
+  DEVICE_VENDOR := Pine64
+  DEVICE_MODEL := PineCube
+  DEVICE_PACKAGES:=kmod-rtc-sunxi
+  SOC := sun8i-s3
+endef
+TARGET_DEVICES += pinecube
